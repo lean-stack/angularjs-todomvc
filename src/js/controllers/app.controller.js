3d4e9e9a611833d382bf3e1e575ce47dd45052f9
Micha Alt <mail@lean-stack.de>
2020-06-11 14:23:43 +0200

    Handling adding todos.

--- src/js/controllers/app.controller.js
+++ src/js/controllers/app.controller.js
@@ -9,7 +9,15 @@
     // We need to focus, cause auto-focussing is blocked by browser due to anchor routes
     document.querySelector('.new-todo').focus();
 
+    // Properties
+    $scope.title = '';
     $scope.todos = todoStore.getAll();
+
+    // Event Handlers
+    $scope.create = function() {
+      var todo = todoStore.addTodo($scope.title);
+      $scope.todos.push(todo);
+    }
   }
 
 })();
--- src/js/services/todo-store.service.js
+++ src/js/services/todo-store.service.js
@@ -9,4 +9,17 @@
     return JSON.parse(localStorage.getItem('todos') || '[]');
   };
 
+  TodoStore.prototype.addTodo = function (title) {
+    var todos = this.getAll();
+    var nextId = JSON.parse(localStorage.getItem('lastId') || '0') + 1;
+
+    var todo = { id: nextId, title: title, completed: false };
+
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+    localStorage.setItem('lastId', nextId);
+
+    return todo;
+  };
+
 })();
